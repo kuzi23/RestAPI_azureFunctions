97e4813725cbdd329f8c31d73a01c521e812c098
Tresor <Tresor@VM0>
2025-04-21 11:24:26 +0000

    create a widget

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="Widgets/{id:int}", methods=["GET"])
+@app.route(route="Widgets/{id:int?}", methods=["GET"])
 
 def WidgetInfo(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -14,5 +14,22 @@ def WidgetInfo(req: func.HttpRequest) -> func.HttpResponse:
         #Handle GET request
         item_id =  req.route_params.get('id')
         return func.HttpResponse(f"Returning you information on item ID: {item_id}", status_code=200)
+    else:
+        return func.HttpResponse("this HTTP method is not supported.", status_code=405)
+    
+
+@app.route(route="Widgets", methods=["POST"])
+def CreateWidget(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    method = req.method
+    if method == 'POST':
+        try:
+            req_body = req.get_json()
+            widget_name= req_body.get('name')
+            #add logic to create the widget
+            return func.HttpResponse(f"Widget '{widget_name}' created successfully", status_code=201)
+        except ValueError:
+            return func.HttpResponse("Invalid input.", status_code=400)
     else:
         return func.HttpResponse("this HTTP method is not supported.", status_code=405)
